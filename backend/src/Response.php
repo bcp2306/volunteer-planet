e4bf4e0db887cca84cb17f4e83301289c485563c
kosminski99 <162216646+kosminski99@users.noreply.github.com>
2024-04-04 02:57:01 +0100

    Update Response.php

--- backend/src/Response.php
+++ backend/src/Response.php
@@ -20,7 +20,7 @@ class Response
         header('Content-Type: application/json');
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-        header('Access-Control-Allow-Headers: Authorization');
+        header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
         if(Request::method() === 'OPTIONS')
         {
